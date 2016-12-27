26970875586baaf917c5f36f03a2ebae48c1cae6
Oleg Slobodskoi <oleg008@gmail.com>
2016-12-27 15:31:43 +0100

    fix colors again

--- src/styles/vars.js
+++ src/styles/vars.js
@@ -17,22 +17,22 @@ export default {
   textColor: base.dark,
   textColorInverse: base.light,
 
-  borderColor: color(base.light).darken(0.06).string(),
+  borderColor: color(base.light).darken(0.06).hex(),
   cardColor: '#fff',
 
-  backgroundLine: color(base.light).darken(0.03).string(),
+  backgroundLine: color(base.light).darken(0.03).hex(),
 
   sidebarBg: base.dark,
-  sidebarBgActive: color(base.dark).darken(0.2).string(),
+  sidebarBgActive: color(base.dark).darken(0.2).hex(),
   sidebarColor: base.light,
   sidebarBorder: color(base.light).alpha(0.15).string(),
-  sidebarShadow: color(base.dark).darken(0.4).string(),
+  sidebarShadow: color(base.dark).darken(0.4).hex(),
 
   // Size variables
   contentWidth: 100,
 
   // Something like SASS functions
-  border: (borderColor = color(base.light).darken(0.06).string()) => ({
+  border: (borderColor = color(base.light).darken(0.06).hex()) => ({
     width: '1px', // jss-default-unit and jss-expand bug. Deep passing interprets 'width' as not a part of 'border' statement
     style: 'solid',
     color: borderColor,
