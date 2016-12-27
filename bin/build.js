c02f45cf8d0407b082f2ea2c02501f9d17b0a665
Oleg Slobodskoi <oleg008@gmail.com>
2016-12-27 13:04:09 +0100

    use babelrc for the html build

--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,9 @@
-require('babel-register')({
-  presets: ["es2015", "react", "stage-0"]
-})
-var ReactDOMServer = require('react-dom/server')
+var fs = require('fs')
+var path = require('path')
+
+var configPath = path.join(process.cwd(), '.babelrc')
+var config = JSON.parse(fs.readFileSync(configPath))
+require('babel-register')(config)
+
 var render = require('../src/server').default
 console.log(render())
