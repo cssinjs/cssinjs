1deda9a0b938daad79003c9874ef51c1402c4655
Oleg Slobodskoi <oleg008@gmail.com>
2016-12-29 10:32:33 +0100

    add basic ga

--- src/server.js
+++ src/server.js
@@ -7,6 +7,16 @@ import baseStyles from './styles/baseStyles'
 
 import config from './config'
 
+const analytics = `
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-89548578-1', 'auto');
+  ga('send', 'pageview');
+`
+
 export default function render() {
   const baseSheet = jssSheet.createStyleSheet(baseStyles)
   const normalize = jssSheet.createStyleSheet(jssNormalize)
@@ -44,6 +54,7 @@ export default function render() {
         </div>
         <script src="vendor.bundle.js" />
         <script src="bundle.js" />
+        {process.env.NODE_ENV === 'production' && <script dangerouslySetInnerHTML={{__html: analytics}} />}
       </body>
     </html>
   )}`
