9a3d104ac0c8eb4a5ef456bf250b7f83b2bd850c
Pavel Davydov <typical000@gmail.com>
2017-01-04 00:19:05 +0200

    Fix look of 404 page

--- src/components/NotFound/styles.js
+++ src/components/NotFound/styles.js
@@ -12,36 +12,11 @@ export default {
     minHeight: 40,
     position: 'relative',
     overflow: 'hidden',
-    color: theme.textColor,
-    background: theme.themeColor,
+    color: theme.titleColor,
     textAlign: 'center',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-
-    // Center radial gradient
-    '&:before': {
-      content: '""',
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: translate('-50%', '-50%'),
-      width: '70%',
-      height: '80%',
-      zIndex: 2,
-      background: 'radial-gradient(ellipse closest-side, rgba(255,255,255,0.5), rgba(255,255,255,0))',
-    },
-
-    // Horizontal line
-    '&:after': {
-      content: '""',
-      position: 'absolute',
-      top: '50%',
-      left: 0,
-      right: 0,
-      height: '1px',
-      background: backgroundLineColor,
-    },
   },
   inner: {
     maxWidth: 100,
@@ -51,19 +26,6 @@ export default {
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'column',
-    borderLeft: theme.border(backgroundLineColor),
-    borderRight: theme.border(backgroundLineColor),
-
-    // Vertical line
-    '&:after': {
-      content: '""',
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      left: '50%',
-      width: '1px',
-      background: backgroundLineColor,
-    },
   },
   title: {
     fontSize: 16,
