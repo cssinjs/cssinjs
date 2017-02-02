9944043688c78d9b0ca10d1af9b21812595dc480
Pavel Davydov <typical000@gmail.com>
2017-02-03 00:08:01 +0200

    Remove react and react-dom. Add aliases on serverside

--- bin/build-html.js
+++ bin/build-html.js
@@ -3,8 +3,11 @@ var path = require('path')
 
 var configPath = path.join(process.cwd(), '.babelrc')
 var config = JSON.parse(fs.readFileSync(configPath))
+
 require('babel-register')(config)
 require('ignore-styles')
+require('module-alias').addAliases(require('../webpack.config.base').resolve.alias)
+
 var render = require('../src/server').default
 var nav = require('../src/utils/navigation')
 
