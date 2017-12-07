1a57559899ee2a15037e6cb3370a8e835101b3a6
Pavel Davydov <typical000@gmail.com>
2017-12-07 21:25:02 +0200

    Fix routing for docs

--- src/docs/routes.js
+++ src/docs/routes.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {IndexRoute, Route} from 'react-router'
+import {docs as config} from 'common/config'
 import App from './components/App'
 import Page from './components/Page/'
 
+/**
+ * Due to moving docs in neted dir there is a difference
+ * between server and client-side routes. So we use 'process'
+ */
+
 export default (
-  <Route path="/" component={App}>
+  <Route path={`/${process.browser ? config.rootDir : ''}`} component={App}>
     <IndexRoute component={Page} />
-    <Route path="/:page" component={Page} />
+    <Route path={`${process.browser ? '' : '/'}:page`} component={Page} />
   </Route>
 )
