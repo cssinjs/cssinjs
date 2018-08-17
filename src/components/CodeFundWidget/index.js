7633daf7e9e572f64c502455123eb6513d8617db
Oleg Isonen <oleg008@gmail.com>
2018-08-17 19:21:56 +0200

    set minHeight, lint, cleanup

--- src/components/CodeFundWidget/index.js
+++ src/components/CodeFundWidget/index.js
@@ -1,28 +1,44 @@
-import React, {Component} from 'react'
-import { browserHistory } from 'react-router'
-
-/**
- * Component, for displaying ethical ad from CodeFund.
+/*
+ * Ethical ad from CodeFund.
  */
-class CodeFundWidget extends Component {
-  componentDidMount() {
-    this.unlisten = browserHistory.listen((location) => {
-      if (location.action == "PUSH") {
-        document.getElementById("codefund_ad").innerHTML = "";
-        window._codefund.serve();
-      }
-    });
-  }
 
-  componentWillUnmount() {
-    this.unlisten();
-  }
+import React, {Component} from 'react'
+import {browserHistory} from 'react-router'
+import injectSheet from 'react-jss'
 
-  render() {
-    return (
-      <div ref="codefund" id="codefund_ad" />
-    );
+const styles = {
+  container: {
+    minHeight: 145
   }
 }
 
-export default CodeFundWidget
+class CodeFundWidget extends Component {
+  static propTypes = {
+    classes: React.PropTypes.object
+  }
+
+  componentDidMount() {
+    this.unlisten = browserHistory.listen((location) => {
+      if (location.action === 'PUSH') {
+        this.node.innerHTML = ''
+        // eslint-disable-next-line no-underscore-dangle
+        window._codefund.serve()
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.unlisten()
+  }
+
+  onRef = (node) => {
+    this.node = node
+  }
+
+  render() {
+    const {classes} = this.props
+    return <div ref={this.onRef} id="codefund_ad" className={classes.container} />
+  }
+}
+
+export default injectSheet(styles)(CodeFundWidget)
