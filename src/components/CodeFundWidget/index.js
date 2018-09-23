c4cf2a8a08cefa5cbc8ff5ce3a8a83bd1f9419f9
Oleg Isonen <oleg008@gmail.com>
2018-09-23 19:52:39 +0200

    trying seedAndDew

--- src/components/CodeFundWidget/index.js
+++ src/components/CodeFundWidget/index.js
@@ -2,49 +2,57 @@
  * Ethical ad from CodeFund.
  */
 
-import React, {Component} from 'react'
-import {browserHistory} from 'react-router'
-import injectSheet from 'react-jss'
-import theme from '../../theme'
+import React, { Component } from "react";
+import { browserHistory } from "react-router";
+import injectSheet from "react-jss";
+import theme from "../../theme";
 
 const styles = {
   container: {
-    minHeight: 145,
+    minHeight: 145
   },
   [theme.media.sm]: {
     container: {
-      display: 'none'
+      display: "none"
     }
   }
-}
+};
 
 class CodeFundWidget extends Component {
   static propTypes = {
     classes: React.PropTypes.object.isRequired
-  }
+  };
 
   componentDidMount() {
-    this.unlisten = browserHistory.listen((location) => {
-      if (location.action === 'PUSH') {
-        this.node.innerHTML = ''
+    this.unlisten = browserHistory.listen(location => {
+      if (location.action === "PUSH") {
+        this.node.innerHTML = "";
         // eslint-disable-next-line no-underscore-dangle
-        window._codefund.serve()
+        window._codefund.serve();
       }
-    })
+    });
   }
 
   componentWillUnmount() {
-    this.unlisten()
+    this.unlisten();
   }
 
-  onRef = (node) => {
-    this.node = node
-  }
+  onRef = node => {
+    this.node = node;
+  };
 
   render() {
-    const {classes} = this.props
-    return <div ref={this.onRef} id="codefund_ad" className={classes.container} />
+    const { classes } = this.props;
+    return (
+      <div
+        ref={this.onRef}
+        id="codefund_ad"
+        // SeedAndDew class
+        // "Since our subscribers provide revenue, we require that projects provide an ad-free experience for them"
+        className={`snd-ad ${classes.container}`}
+      />
+    );
   }
 }
 
-export default injectSheet(styles)(CodeFundWidget)
+export default injectSheet(styles)(CodeFundWidget);
--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import {renderToString} from 'react-dom/server'
-import {match, RouterContext} from 'react-router'
-import {stripIndents} from 'common-tags'
-import {minify} from 'html-minifier'
-import {SheetsRegistry} from 'react-jss'
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { match, RouterContext } from "react-router";
+import { stripIndents } from "common-tags";
+import { minify } from "html-minifier";
+import { SheetsRegistry } from "react-jss";
 
-import {version} from '../package.json'
-import routes from './routes'
-import config from './config'
+import { version } from "../package.json";
+import routes from "./routes";
+import config from "./config";
 
 const minifyOptions = {
   minifyCSS: true,
   minifyJS: true
-}
+};
 
-const renderAnalytics = () => (
+const renderAnalytics = () =>
   stripIndents`
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -25,10 +25,9 @@ const renderAnalytics = () => (
       ga('create', 'UA-89548578-1', 'auto');
       ga('send', 'pageview');
     </script>
-  `
-)
+  `;
 
-const renderSidecar = () => (
+const renderSidecar = () =>
   stripIndents`
     <script>
       ((window.gitter = {}).chat = {}).options = {
@@ -36,29 +35,52 @@ const renderSidecar = () => (
       };
     </script>
     <script src="https://sidecar.gitter.im/dist/sidecar.v1.js" async defer></script>
-  `
-)
+  `;
 
-const renderCodeFund = () => (
+const renderCodeFund = () =>
   stripIndents`
     <script src="https://codefund.io/scripts/0c702cf1-8730-4cb2-bb1b-0a36c437e46d/embed.js?theme=codesandbox&template=default" async defer></script>
-  `
-)
+  `;
 
-const renderDoc = ({app, css, analytics, sidecar, codeFund}) => (
+// If removing this, also remove the class from CodeFundWidget.
+const renderSeedAndDev = () => stripIndents`
+  <script>
+    var SeedAndDewConfig = {};
+    (function() {
+      SeedAndDewConfig['adClass'] = "snd-ad";
+      /* * * DON'T EDIT BELOW THIS LINE * * */
+      SeedAndDewConfig['projectId'] = 'db61e071-7302-4021-a7ac-45171be0bd7b';
+      SeedAndDewConfig['loadStartTime'] = performance.now();
+      SeedAndDewConfig['apiVersion'] = '2018-05-28'
+      SeedAndDewConfig['sessionId'] = Math.random().toString(36).substring(2, 15);
+      var snd = document.createElement('script');
+      snd.type = 'text/javascript';
+      snd.async = true;
+      snd.src = 'https://www.seedanddew.com/static/embed.min.js';
+      (document.getElementsByTagName('head')[0] ||
+      document.getElementsByTagName('body')[0]).appendChild(snd);
+    })();
+  </script>
+`;
+
+const renderDoc = ({ app, css, analytics, sidecar, codeFund, seedAndDew }) =>
   stripIndents`
     <!doctype html>
     <html lang="en">
       <head>
         <title>${config.site.head.title}</title>
         <meta name="description" content="${config.site.head.description}" />
-        <meta name="keywords" content="${config.site.head.keywords.join(' ')}" />
+        <meta name="keywords" content="${config.site.head.keywords.join(
+          " "
+        )}" />
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta httpEquiv="Content-Language" content="en" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="${config.site.og.title}" />
-        <meta property="og:description" content="${config.site.og.description}" />
+        <meta property="og:description" content="${
+          config.site.og.description
+        }" />
         <meta property="og:type" content="${config.site.og.type}" />
         <meta property="og:image" content="${config.site.og.image}" />
         <meta property="og:url" content="${config.site.og.url}" />
@@ -75,21 +97,25 @@ const renderDoc = ({app, css, analytics, sidecar, codeFund}) => (
         ${codeFund}
         ${analytics}
         ${sidecar}
+        ${seedAndDew}
       </body>
     </html>
-  `
-)
+  `;
 
 export default (location, callback) => {
-  const registry = new SheetsRegistry()
-  match({routes: routes({registry}), location}, (error, redirectLocation, renderProps) => {
-    const html = renderDoc({
-      app: renderToString(<RouterContext {...renderProps} />),
-      css: registry.toString(),
-      analytics: renderAnalytics(),
-      sidecar: renderSidecar(),
-      codeFund: renderCodeFund()
-    })
-    callback(minify(html, minifyOptions))
-  })
-}
+  const registry = new SheetsRegistry();
+  match(
+    { routes: routes({ registry }), location },
+    (error, redirectLocation, renderProps) => {
+      const html = renderDoc({
+        app: renderToString(<RouterContext {...renderProps} />),
+        css: registry.toString(),
+        analytics: renderAnalytics(),
+        sidecar: renderSidecar(),
+        codeFund: renderCodeFund(),
+        seedAndDew: renderSeedAndDev()
+      });
+      callback(minify(html, minifyOptions));
+    }
+  );
+};
