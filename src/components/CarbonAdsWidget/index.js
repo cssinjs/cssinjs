93c16335e69d1160239300cfac7fe357e9db7827
Oleg Isonen <oleg008@gmail.com>
2020-12-07 14:12:19 +0100

    carbon css

--- src/components/CarbonAdsWidget/index.js
+++ src/components/CarbonAdsWidget/index.js
@@ -10,19 +10,59 @@ import theme from '../../theme'
 const styles = {
   container: {
     padding: 5,
-    minHeight: 100,
-    '& img': {
-      float: 'left',
-      marginRight: 10
-    },
-    '& a': {
-      cursor: 'pointer',
-      color: '#eee'
-    }
+    minHeight: 120,
+    flexShrink: 0
   },
   [theme.media.sm]: {
     container: {
       display: 'none'
+    }
+  },
+  '@global': {
+    '#carbonads': {
+      display: 'block',
+      fontFamily: 'Verdana, "Helvetica Neue", Helvetica, sans-serif',
+      lineHeight: 1.35,
+      overflow: 'hidden',
+      '& span': {
+        display: 'block',
+        position: 'relative',
+        overflow: 'hidden'
+      },
+      '& .carbon-img': {
+        float: 'left',
+        marginRight: '0.5em'
+      },
+
+      '& .carbon-img img': {
+        display: 'block',
+        margin: 0
+      },
+
+      '& a.carbon-text': {
+        display: 'block',
+        float: 'left',
+        marginBottom: 10,
+
+        maxWidth: 'calc(100% - 130px - 1em)',
+        textAlign: 'left',
+        textDecoration: 'none',
+        color: '#eee',
+        cursor: 'pointer'
+      },
+
+      '& a.carbon-poweredby': {
+        display: 'block',
+        position: 'absolute',
+        bottom: '0',
+        left: 146,
+
+        fontSize: 10,
+        lineHeight: 1,
+        letterSpacing: 1,
+        textTransform: 'uppercase',
+        cursor: 'pointer'
+      }
     }
   }
 }
